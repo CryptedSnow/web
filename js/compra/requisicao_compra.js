17a27908a5707bf718da5b9128a4779f073983f4
Iury189 <63120240+Iury189@users.noreply.github.com>
2021-02-13 13:59:22 -0300

    Add files via upload

---
+++ js/compra/requisicao_compra.js
@@ -0,0 +1,33 @@
+// JS que faz a requisicao de dados
+function buscaDados(){
+    // Variavel cd_compra_fornecedor que retorna o elemento cd_compra_fornecedor
+    var cd_compra_fornecedor = document.querySelector("#cd_compra_fornecedor").value;
+    // Verifica se o option selecionado e vazio (value="")
+    if(!cd_compra_fornecedor){
+    	// Apaga os valores dos elementos do formulario
+      document.forms[0].reset();
+      // Aborta o resto da funcao
+      return;
+   	}
+    // Instancia a classe XMLHttpReques
+    ajax = new XMLHttpRequest();
+    // Especifica o Method e a url que sera chamada
+    ajax.open("GET","/WEB/json/id_compra.php?cd_compra_fornecedor="+cd_compra_fornecedor,true);
+    // Executa na resposta do ajax
+    ajax.onreadystatechange = function(){
+    	// Se completar a requisicao
+	    if(ajax.readyState == 4){
+	        // Se retornar
+		    if(ajax.status == 200){
+		      // Converte a string retornada para dados em JSON no JS
+		      var retornoJson = JSON.parse(ajax.responseText);
+		      // Preenche os campos com o retorno dos dados em cada campo
+		      document.querySelector("#cd_compra_fornecedor").value = retornoJson[0].cd_compra_fornecedor;
+		      document.querySelector("#cd_fornecedor").value = retornoJson[0].cd_fornecedor;
+		      document.querySelector("#cd_produto").value = retornoJson[0].cd_produto;
+		    }
+	    }
+    }
+  // Envia a solicitacao
+  ajax.send();
+}
