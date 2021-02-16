fbf776591d400d13d6d9b39486b877a294268fa6
Iury189 <63120240+Iury189@users.noreply.github.com>
2021-02-16 10:22:20 -0300

    Add files via upload

--- js/compra/requisicao_compra.js
+++ js/compra/requisicao_compra.js
@@ -12,7 +12,7 @@ function buscaDados(){
     // Instancia a classe XMLHttpReques
     ajax = new XMLHttpRequest();
     // Especifica o Method e a url que sera chamada
-    ajax.open("GET","/WEB/json/id_compra.php?cd_compra_fornecedor="+cd_compra_fornecedor,true);
+    ajax.open("GET","/web/json/id_compra.php?cd_compra_fornecedor="+cd_compra_fornecedor,true);
     // Executa na resposta do ajax
     ajax.onreadystatechange = function(){
     	// Se completar a requisicao
