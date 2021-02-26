b07b5bf9836c3a71d9f013746ab200fec333036e
Iury189 <63120240+Iury189@users.noreply.github.com>
2021-02-26 12:13:49 -0300

    Add files via upload

--- js/produto/mascara_produto.js
+++ js/produto/mascara_produto.js
@@ -1,6 +1,5 @@
 // JS para formatar as máscaras nos campos
 $(document).ready(function(){
 	$("#codigo").mask("0-000000-000000")
-	$("#quantidade").mask("0000")
 	$("#valor_compra").mask("00000.00", {reverse: true})
 })
