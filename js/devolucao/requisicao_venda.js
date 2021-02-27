4c29d6b2c58299d1dc196e9058e5f72d6338b1b0
Iury189 <63120240+Iury189@users.noreply.github.com>
2021-02-27 13:26:14 -0300

    Add files via upload

--- js/devolucao/requisicao_venda.js
+++ js/devolucao/requisicao_venda.js
@@ -25,18 +25,6 @@ function buscaDados() {
         		document.querySelector("#cd_produto").value = retornoJson[0].cd_produto;
         		document.querySelector("#valor_item").value = retornoJson[0].valor_item;
         		document.querySelector("#quantidade").value = retornoJson[0].quantidade;
-        		let elemento = document.getElementById("quantidade");
-        		elemento.innerHTML = "";
-          		// Loop que cria varios options no campo quantidade
-          		for (let i = 0; i <= retornoJson[0].quantidade; i++) {
-		            let option = document.createElement("option");
-		            option.text = option.elemento = i;
-	            	if (i == 0) {
-	              		option.setAttribute("title","Por padrão a opção é zero, escolha abaixo a quantidade desejada.");
-	            	}
-		            option.setAttribute("value", i);
-		            elemento.appendChild(option);
-		        }
     		}
 		}
 	}
