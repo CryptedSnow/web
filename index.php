d81adb6cb24626086856dcb3ad7fff16dfce8695
Iury189 <63120240+Iury189@users.noreply.github.com>
2021-03-11 11:29:23 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 		echo "<script> alert('Você já está logado! faça logout para sair do sistema.'); location.href='/web/inicio.php' </script>";
 		die;
 	}
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
