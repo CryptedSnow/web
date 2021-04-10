ce454f608355865914eee34586dd5b153d21ef04
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-04-10 10:43:37 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if((!isset($_SESSION['id_usuario'])) || (empty($_SESSION['id_usuario']))){
+    if(!isset($_SESSION['id_usuario'])){
 		header("Location: index.php");
 		die;
 	}
